4412363577025e7ec678e8dcfea1154c445a58d7
Stephane ANDRE <stephane.andre85@gmail.com>
2024-04-02 10:50:32 +0200

    adding rule into eslintrc.json

--- notflix/pages/profiles.tsx
+++ notflix/pages/profiles.tsx
@@ -35,7 +35,7 @@ const Profiles = () => {
     return (
         <div className="flex items-center h-full justify-center">
             <div className="flex flex-col">
-                <h1 className="text-3xl md:text-6xl text-white text-center"> Who's watching Notflix?</h1>
+                <h1 className="text-3xl md:text-6xl text-white text-center"> Who is watching Notflix?</h1>
                 <div className="flex items-center justify-center gap-8 mt-10">
                     {/* Profile component */}
                     <div onClick={() => router.push("/")}>
